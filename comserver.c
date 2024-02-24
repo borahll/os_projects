0ad8174be3f7e2350990ee0ea13f26963b15f2c8
Bhll <bora.haliloglu@ug.bilkent.edu.tr>
2024-02-24 20:52:29 +0300

    ss

--- comserver.c
+++ comserver.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
         // Parse the received message to obtain client's named pipe names and WSIZE
         char csPipeName[100], scPipeName[100];
         int wSize;
-        sscanf(buffer, "%s %s %d", csPipeName, scPipeName, &wSize);
+        sscanf(buffer, "%s %s %d", scPipeName, csPipeName, &wSize);
 
         // Fork a new process to handle the client
         pid_t pid = fork();
