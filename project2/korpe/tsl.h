1b9d64c518a0df603f2378fb4b6fea23bca897b3
Bhll <bora.haliloglu@ug.bilkent.edu.tr>
2024-03-24 18:21:44 +0300

    Revert "Update tsl.h"
    This reverts commit 09de5b63963cddb1bfa0dfb43f860fc176515f12.

--- project2/korpe/tsl.h
+++ project2/korpe/tsl.h
@@ -1,54 +1,33 @@
 #ifndef _TSL_H_
 #define _TSL_H_
 
-#include <ucontext.h>
-#include <stdbool.h>
+// do not change this header file (tsl.h) 
+// it is the interface of the tsl library to the applications
 
-typedef enum { ALG_FCFS = 1,  ALG_RANDOM = 2} SchedulingAlgorithm;
-typedef enum { READY, RUNNING, TERMINATED } ThreadState;
+
+
 
 #define TSL_MAXTHREADS 256 // maximum number of threads (including the main thread) that an application can have.
-#define TSL_STACKSIZE  32768 // bytes, i.e., 32 KB. This is the stack size for a new thread.
+#define TSL_STACKSIZE  32768 // bytes, i.e., 32 KB. This is the stack size for a new thread. 
 
-#define ALG_MYALGORITHM 3
+#define ALG_FCFS 1
+#define ALG_RANDOM 2
+#define ALG_RR 3
 
-#define TID_MAIN 0 // tid of the main thread. This id is reserved for the main thread.
+#define TID_MAIN 1 // tid of the main tread. this id is reserved for main thread.
 
-#define TSL_ANY 0  // yield to a thread selected with a scheduling algorithm.
+#define TSL_ANY 0  // yield to a thread selected with a scheduling alg.
 
 #define TSL_ERROR  -1  // there is an error in the function execution.
 #define TSL_SUCCESS 0  // function execution success
 
-typedef struct ThreadControlBlock {
-    ucontext_t context;
-    int tid;
-    bool isActive;  // Indicates if the thread slot is used
-    ThreadState state;
-    void* stack;
-    //int estimatedRuntime; // For SJF and SRTF
-} ThreadControlBlock;
-
-typedef struct Scheduler {
-    SchedulingAlgorithm algorithm;
-    ThreadControlBlock* threads[TSL_MAXTHREADS];
-    int currentThreadIndex;
-    int threadCount;
-    ThreadControlBlock* runqueue[TSL_MAXTHREADS]; //list of ready queues 
-    int runqueueCount;
-    int currentReadyThread;
-} Scheduler;
 
 int tsl_init(int salg);
-int tsl_create_thread(void (*tsf)(void *), void *targ);
-int tsl_yield(int tid);
-void scheduler_init(SchedulingAlgorithm alg);
-int scheduler_next_thread();
-int tsl_create_thread(void (*tsf)(void *), void *targ);
+int tsl_create_thread (void (*tsf)(void *), void *targ);
+int tsl_yield (int tid);
+int tsl_exit();
 int tsl_join(int tid);
-void tsl_exit();
 int tsl_cancel(int tid);
-int tsl_gettid(); 
-
+int tsl_gettid();
 
 #endif
-
